6c81c47df4727c6ccd8833f1c5d6f42126f3dbc1
arzola <oscararzola@gmail.com>
2023-02-06 16:09:15 -0500

    fix: safe directory

--- update/app/Commands/UpdateCommand.php
+++ update/app/Commands/UpdateCommand.php
@@ -179,11 +179,11 @@ class UpdateCommand extends Command
     protected function output(string $output): void
     {
         $this->out = Str::of($output)
-                ->explode(PHP_EOL)
-                ->filter(fn($item) => Str::contains($item, ' - '))
-                ->reject(fn($item) => Str::contains($item, 'Downloading '))
-                ->takeUntil(fn($item) => Str::contains($item, ':'))
-                ->implode(PHP_EOL).PHP_EOL;
+                        ->explode(PHP_EOL)
+                        ->filter(fn ($item) => Str::contains($item, ' - '))
+                        ->reject(fn ($item) => Str::contains($item, 'Downloading '))
+                        ->takeUntil(fn ($item) => Str::contains($item, ':'))
+                        ->implode(PHP_EOL).PHP_EOL;
 
         $this->line($this->out);
     }
@@ -198,8 +198,8 @@ class UpdateCommand extends Command
         $this->info('Committing changes ...');
 
         Git::addAllChanges()
-            ->commit(env('GIT_COMMIT_PREFIX', '').'composer update '.today()->toDateString().PHP_EOL.PHP_EOL.$this->out)
-            ->push(['origin', $this->new_branch]);
+           ->commit(env('GIT_COMMIT_PREFIX', '').'composer update '.today()->toDateString().PHP_EOL.PHP_EOL.$this->out)
+           ->push(['origin', $this->new_branch]);
     }
 
     /**
